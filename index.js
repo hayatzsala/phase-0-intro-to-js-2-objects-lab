f648df7a33e51eb6584c398d2a53c7e77847835b
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-08-12 03:51:59 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "hayat",
+    streetAddress : "ersal"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    var newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    var newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
